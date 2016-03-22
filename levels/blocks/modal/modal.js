d2d7bad120d21b78cec6930eed4e0fe6f3c0eeb0
Urkass <Kuznetpa@gmail.com>
2016-03-22 03:00:08 +0300

    svg

--- levels/blocks/modal/modal.js
+++ levels/blocks/modal/modal.js
@@ -1,13 +1,12 @@
-_button = $('#button');
-_modal = $('.modal');
-_modalError = $('.modal_type_error')[0];
-_modalOrder = $('.modal_type_order')[0];
-_modalSuccess = $('.modal_type_success')[0];
-_close = $('.modal__close');
-_form = $('.modal__form'); //массив форм 0 - форма главная, 1 - успех, 2 - неудача
 
-_button.on( 'click', ModalOpen);
-_close.on('click', ModalClose);
+var _modal = $('.modal');
+
+$('#button').on( 'click', function(){
+    ModalOpen($('.modal_type_order'));
+});
+$('.modal__close').on('click', function(){
+    ModalClose(_modal);
+});
 $(window).on('click', function(e){
     if ($(e.target).is(_modal)) {
         ModalClose(_modal);
@@ -24,27 +23,34 @@ function ModalClose(modal){
 }
 
 function showOrderModal(_id){
+    var contentId = $('#contentId');
     var request = 'requestProduct';
     if(!_id) {
         _id = 0;
         request = 'requestCallback';
     }
-    $('#id-input').value = _id;
-    $('#id-input').val(_id);
-    _form.on ('submit', function(){
+    contentId.attr('value', _id);
+    contentId.val(_id);
+    $('.modal__form').on ('submit', function(){
         sendAjaxModal();
     });
     ModalOpen();
-};
+}
 
 function sendAjaxModal(){
     $.ajax({
-        url: locationPath + contextPath + "/mail/" + request,
+        url: ''/*locationPath + contextPath + "/mail/" + request*/,
         type:'get',
         data: $("#mod-form").serialize(),
-        complete: function(){
-            console.log('SUCCESS');
-            showSuccessModal();
+        complete: function() {
+            ModalClose(_modal);
+            ModalOpen($('.modal_type_success'));
+
+        },
+        error: function() {
+            ModalClose(_modal);
+            ModalOpen($('.modal_type_error'));
+
         }
     });
-};
+}
